5cd68e4984341ef2d83a6b845526ac46d1139ec5
DrakeDaemon <drakedaemon1@gmail.com>
2025-02-04 22:29:22 +0300

    Game (almost) done

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import json
+import time
 
-# Reading JSONs
-listof = "list.json"
-with open(listof, "r") as f:
-    questions = json.load(f)
-    
-print (questions)
+
+# * Game
+with open('list.json', "r") as f:   # Json reader
+    questions = json.load(f)    
+ # ! BAD CODE NEEDS REWORKING
+print(questions[0]['prompt'])
+answer = input(questions[0]['options'])
+
+if answer == questions[0]['answer']: # Game itself
+    print('Correct')
+    print(questions[1]['prompt'])
+    answer = input(questions[1]['options'])
+    if answer == questions[1]['answer']:
+        print('Correct')
+    else:
+        print('Incorrect')
+else: 
+    print('Incorrect')    
+
+# * GUI
