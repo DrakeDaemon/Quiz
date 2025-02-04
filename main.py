2eaa4d1d63ad99cd1d9aa56b5bb1fe7efce2a54d
DrakeDaemon <drakedaemon1@gmail.com>
2025-02-04 22:59:47 +0300

    added randomness

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import json
 import time
+import random as rand
 
 
 # * Game
-with open('list.json', "r") as f:   # Json reader
-    questions = json.load(f)    
- # ! BAD CODE NEEDS REWORKING
-print(questions[0]['prompt'])
-answer = input(questions[0]['options'])
+with open('questions.json', "r") as f:   # Json reader
+    questions = json.load(f)
 
-if answer == questions[0]['answer']: # Game itself
+# ! BAD CODE NEEDS REWORKING
+random = rand.choice(questions)
+random1 = rand.choice(questions) 
+quest = random['A'],random['B'],random['C'],random['D']
+quest1 = random1['A'],random1['B'],random1['C'],random1['D']
+print(random['prompt'])
+answer = input(quest)
+
+if answer == random['answer']: # Game itself
     print('Correct')
-    print(questions[1]['prompt'])
-    answer = input(questions[1]['options'])
-    if answer == questions[1]['answer']:
+    print(random1['prompt'])
+    answer = input(quest1)
+    if answer == random1['answer']:
         print('Correct')
     else:
         print('Incorrect')
